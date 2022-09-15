bf2ca37f428d67f50db583c1d873471a29bd5aca
Jaroslav-89 <jaroslav89@bk.ru>
2022-09-15 17:00:35 +0400

    creating project

---
+++ app/src/main/java/com/jar89/myfirstgitproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jar89.myfirstgitproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
